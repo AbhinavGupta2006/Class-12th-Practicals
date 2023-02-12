992810e383c65cd95f0a551ae393f28af4e68105
Abhi <meeradevi5313@gmail.com>
2023-02-12 23:53:41 +0530

    removed extra function

--- prac-5.py
+++ prac-5.py
@@ -1,12 +1,10 @@
 def listSearch(Thelist,element):
-    def lElement(element):
-        frequency=Thelist.count(element)
-        print('Frequency of',element,'in list is:',frequency)
-        print('locations of',element,':',end='')
-        for i in range(len(Thelist)):
-            if Thelist[i]==element:
-                print(i)
-    return lElement(element)
+    frequency=Thelist.count(element)
+    print('Frequency of',element,'in list is:',frequency)
+    print('locations of',element,':')
+    for i in range(len(Thelist)):
+        if Thelist[i]==element:
+            print(i)
 asklist=eval(input('Enter the list: '))
 elmnt=int(input('Enter the element name to search for:'))
 print(listSearch(asklist,elmnt))
